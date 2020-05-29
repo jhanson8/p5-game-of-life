259f23284a194ad86c5a456782b9b774c2edd5f3
Jonathan Hanson <jhanson8@uoregon.edu>
2020-05-28 19:58:08 -0700

    major updates

--- game-of-life-p5/sketch.js
+++ game-of-life-p5/sketch.js
@@ -6,72 +6,209 @@ function make2DArray(cols, rows) {
 	return arr;
   }
   
+  
   let grid;
   let cols;
   let rows;
-  let resolution = 20;
+  let res = 20;
+  let fr = 14
+  let value = 0
+  let isRunning = 0
+  let play = false 
+
   
   function setup() {
+	
 	createCanvas(500, 500);
-	cols = width / resolution;
-	rows = height / resolution;
+	cols = width / res;
+	rows = height / res;
+
+	space = min(width,height) / max(rows, cols);
+	xoffset = width/2 - space * cols * 0.5;
   
 	grid = make2DArray(cols, rows);
-	for (let i = 0; i < cols; i++) {
-	  for (let j = 0; j < rows; j++) {
-		grid[i][j] = 0
-	  }
-	}
-  }
-  
-  function draw() {
-	background(0);
-  
-	for (let i = 0; i < cols; i++) {
-	  for (let j = 0; j < rows; j++) {
-		let x = i * resolution;
-		let y = j * resolution;
-		if (grid[i][j] == 1) {
-		  fill(255);
-		  stroke(0);
-		  rect(x, y, resolution - 1, resolution - 1);
-		}
-	  }
-	}
 
-// to click on individual squares
-	function clicked() {
-		for(let i = 0; i < cols; i++){
-			for(let j = 0; j < rows; j++){
-				var dis = dist(mouseX, mouseY, x[i], y[j]);
-				if(dist < resolution/2) col[j * 25 + i] =! col[j * 25 + i]
-			}
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+		  grid[i][j] = 0
+		}
+	  }
+
+
+	  startButton = createButton("Start")
+	  startButton.mousePressed(start)
+	
+	  endButton = createButton("Stop")
+	  endButton.mousePressed(stop)
+
+	  singleFrameButton = createButton("Single Frame")
+	  singleFrameButton.mousePressed(singleFrame)
+	
+	  randomButton = createButton("Random")
+	  randomButton.mousePressed(randomGrid)
+
+	  resetButton = createButton("Clear")
+	  resetButton.mousePressed(clearGrid)
+
+	  increaseFRButton = createButton("Increase FR")
+	  increaseFRButton.mousePressed(increaseFR)
+	 
+	  decreaseFRButton = createButton("Decrease FR")
+	  decreaseFRButton.mousePressed(decreaseFR)
+
+	  resetButton = createButton("Reset")
+	  resetButton.mousePressed(reset)
+
+	
+	noLoop()
+  }
+
+
+
+  function mousePressed(){
+	if(!isRunning){
+		selectX = Math.floor((mouseX-xoffset) / space);
+	selectY = Math.floor(mouseY / space);
+	if(grid[selectX][selectY] == 1){
+	  grid[selectX][selectY] = 0;
+	  redraw()
+	 
+	} else {
+	  grid[selectX][selectY] = 1;
+	  redraw()
+	 
+	}
+	console.log(`x: ${selectX}\ny: ${selectY}`);
+
+	}
+	
+  }
+
+ 
+
+ function increaseFR(){
+	fr++;
+ }
+ function decreaseFR(){
+ 	fr--;
+}
+ 
+
+
+
+  function randomGrid(){
+
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+		  grid[i][j] = 0
+		}
+	  }
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+		  grid[i][j] = Math.round((Math.random()));
+		}
+	  }
+	noLoop()
+
+	redraw()
+  }
+
+  function clearGrid(){
+		for (let i = 0; i < cols; i++) {
+			for (let j = 0; j < rows; j++) {
+		  		grid[i][j] = 0
+		}
+	  }
+	loop()
+	// noLoop()
+  }
+
+
+
+  
+
+
+  function start(){
+	isRunning = 1;
+	frameRate(fr);
+	loop()
+  }
+
+  function stop(){
+    noLoop()
+  }
+
+  function singleFrame(){
+	redraw()
+  }
+
+  function reset(){
+	 location.reload() 
+  }
+
+
+
+function draw() {
+	frameRate(fr)
+	background(0);
+	text(frameCount, width/2, height);
+	if(isRunning == 0){
+	  for (let i = 0; i < cols; i++){
+		for (let j = 0; j < rows; j++){
+		  let x = i * res;
+		  let y = j * res;
+		  stroke(0); //grid color
+		  fill(0);       //grid fill (dead)
+		  rect(x, y, res - 1 , res - 1);
+		  if(grid[i][j] == 1){
+			fill(240, 255, 13); //grid alive
+			rect(x, y, res - 1 , res - 1);
+		  } 
 		}
 	}
+  }
   
-	let next = make2DArray(cols, rows);
-  
-	// Compute next based on grid
-	for (let i = 0; i < cols; i++) {
-	  for (let j = 0; j < rows; j++) {
-		let state = grid[i][j];
-		// Count live neighbors!
-		let sum = 0;
-		let neighbors = countNeighbors(grid, i, j);
-  
-		if (state == 0 && neighbors == 3) {
-		  next[i][j] = 1;
-		} else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
-		  next[i][j] = 0;
-		} else {
-		  next[i][j] = state;
-		}
+	else if(isRunning == 1){
+	for (let i = 0; i < cols; i++){
+	  for (let j = 0; j < rows; j++){
+		let x = i * res;
+		let y = j * res;
+		stroke(0); //grid color
+		fill(0);       //grid fill (dead)
+		rect(x, y, res - 1 , res - 1);
+		if(grid[i][j] == 1){
+		  fill(240, 255, 13); //grid alive
+		  rect(x, y, res - 1 , res - 1);
+		} 
 	  }
 	}
   
-	grid = next;
-  }
+	
   
+	  let next = make2DArray(cols, rows);
+  
+	//compute next based on grid
+	  for(let i = 0; i < cols; i++){
+		for(let j = 0; j < cols; j++){
+		  let state = grid[i][j];
+		  let sum = 0;
+		  let neighbors = countNeighbors(grid, i, j);
+  
+		  if (state == 0 && neighbors == 3){
+			next[i][j] = 1;
+		  } else if (state == 1 && neighbors < 2 || neighbors > 3){
+			next[i][j] = 0;
+		  }else{
+			next[i][j] = state;
+		  }
+	  }
+	}
+  
+  grid = next;
+  }}
+
+
+
   function countNeighbors(grid, x, y) {
 	let sum = 0;
 	for (let i = -1; i < 2; i++) {
@@ -84,4 +221,8 @@ function make2DArray(cols, rows) {
 	sum -= grid[x][y];
 	return sum;
   }
-  
+ 
+
+
+
+
